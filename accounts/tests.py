63e84b23b05039f08639565b86dec033be9ee580
Roberto <y.roberto@ymail.com>
2021-08-13 11:07:32 -0300

    CI configuration

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import CustomUser
 
-# Create your tests here.
+def test_user_model_used():
+    assert get_user_model() == CustomUser
