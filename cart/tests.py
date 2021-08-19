6baa7e56a7f181d07085e27ffcb6efab0072c963
Roberto <y.roberto@ymail.com>
2021-08-19 09:33:08 -0300

    added context processor

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.http import request
+import pytest
+from pytest_django.asserts import assertContains
+from shop.models import Product
+from cart.cart import Cart
 
-# Create your tests here.
+# @pytest.fixture
+# def sample_product(db):
+#     product = Product.objects.get()
+#     return product
+
+
+# def test_cart_add(db,sample_product):
+#     cart = Cart(request)
+#     cart.add(sample_product)
+#     assert cart[1]
+
