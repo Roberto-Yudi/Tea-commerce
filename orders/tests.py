37575aca8844ac9f122ef4fb4ee0a2433e3973e5
Roberto <y.roberto@ymail.com>
2021-08-30 08:33:27 -0300

    Revert "installed celery"
    This reverts commit 082a446880aac3cfe8f21f2aa07339d1af03255c.

--- orders/tests.py
+++ orders/tests.py
@@ -1,25 +1,3 @@
-import pytest
-from model_mommy import mommy
-from pytest_django.asserts import assertContains
-from .forms import OrderCreateForm
-from .models import Order
+from django.test import TestCase
 
-@pytest.fixture
-def order(db):
-    order = mommy.make(Order)
-    return order
-
-def test_order_model(order):
-   assert Order.first_name 
-   assert Order.last_name 
-   assert Order.email 
-   assert Order.address
-   assert Order.postal_code 
-   assert Order.city 
-   assert Order.created 
-   assert Order.updated 
-   assert Order.paid
-
-    
-
-
+# Create your tests here.
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-from orders.tasks import order_created
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -15,7 +13,6 @@ def order_create(request):
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
             
             cart.clear()
-            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order':order})
     else:
         form = OrderCreateForm()
